2416b0aa1ae22ee9abc5fcd35d35fcbe70f47c3c
Christopher N. Hesse <raymanfx@gmail.com>
2022-08-05 21:55:02 +0200

    array: Support slicing Matrix instances
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- tinynn/src/array/matrix.rs
+++ tinynn/src/array/matrix.rs
@@ -1,6 +1,6 @@
 use std::{
     mem,
-    ops::{Deref, DerefMut, Index, IndexMut},
+    ops::{Deref, DerefMut, Index, IndexMut, Range},
 };
 
 use crate::array::Array;
@@ -13,7 +13,31 @@ pub struct Matrix<A> {
     pub(crate) array: A,
 }
 
-impl<T> Matrix<&Array<T>> {
+impl<T> Matrix<&Array<T>>
+where
+    T: Copy,
+{
+    /// Returns a partial view of the original matrix.
+    ///
+    /// # Arguments
+    ///
+    /// * `range` - Range specifying which rows to expose
+    pub fn slice(&self, range: Range<usize>) -> Array<T> {
+        assert!(range.start < self.array.shape[0]);
+        assert!(range.end <= self.array.shape[0]);
+        let buf: Vec<T> = self
+            .rows()
+            .enumerate()
+            .filter(|&(i, _)| i >= range.start && i < range.end)
+            .map(&|(_, x)| x)
+            .fold(Vec::new(), |mut accum, row| {
+                accum.extend(row);
+                accum
+            });
+        let shape = vec![range.len(), self.array.shape[1]];
+        Array { buf, shape }
+    }
+
     // Returns the rows of the matrix.
     pub fn rows(&self) -> impl Iterator<Item = &[T]> {
         let rows = self.array.shape[0];
@@ -25,7 +49,41 @@ impl<T> Matrix<&Array<T>> {
     }
 }
 
-impl<T> Matrix<&mut Array<T>> {
+impl<T> Matrix<&mut Array<T>>
+where
+    T: Copy,
+{
+    /// Returns a partial view of the original matrix, allowing for mutation.
+    ///
+    /// # Arguments
+    ///
+    /// * `range` - Range specifying which rows to expose
+    pub fn slice(&self, range: Range<usize>) -> Array<T> {
+        assert!(range.start < self.array.shape[0]);
+        assert!(range.end <= self.array.shape[0]);
+        let buf: Vec<T> = self
+            .rows()
+            .enumerate()
+            .filter(|&(i, _)| i >= range.start && i < range.end)
+            .map(&|(_, x)| x)
+            .fold(Vec::new(), |mut accum, row| {
+                accum.extend(row);
+                accum
+            });
+        let shape = vec![range.len(), self.array.shape[1]];
+        Array { buf, shape }
+    }
+
+    // Returns the rows of the matrix.
+    pub fn rows(&self) -> impl Iterator<Item = &[T]> {
+        let rows = self.array.shape[0];
+        let mut slices = Vec::new();
+        for i in 0..rows {
+            slices.push(&self[i]);
+        }
+        slices.into_iter()
+    }
+
     // Returns the rows of the matrix, allowing for mutation.
     pub fn rows_mut(&mut self) -> impl Iterator<Item = &mut [T]> {
         let rows = self.array.shape[0];
